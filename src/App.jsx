0018fc0b0c5979dc11c385a33bcbf59f2c11e742
subodhinigurav28 <96240065+subodhinigurav28@users.noreply.github.com>
2024-02-10 23:13:50 +0530

    Created header

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
-import React from 'react';
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from './pages/Home';
+import About from './pages/About';
+import SignUp from "./pages/SignUp";
+import Profile from "./pages/Profile";
+import Login from "./pages/Login";
+import Header from "./Components/Header";
 export default function App() {
-  return (
-    <h1 className='text-red-500'>
-      App
-    </h1>
-  );
+  return <BrowserRouter>
+  <Header /> 
+  <Routes>
+    <Route path="/" element={<Home />} />
+    <Route path="/sign-up" element={<SignUp />} />
+    <Route path="/about" element={<About />} />
+    <Route path="/profile" element={<Profile />} />
+    <Route path="/login" element={<Login />} />
+  </Routes>
+  </BrowserRouter>;
+    
+  
 }
+
